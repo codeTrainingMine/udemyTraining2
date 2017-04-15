362ae37293605b0b435194fc2c126e27efe9187d
codeTrainingMine <enhilex.linux@gmail.com>
2017-04-16 08:38:04 +1000

    adds files to 05, 06

---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const p string = "death & taxes"
+
+func main() {
+
+	const q int = 42
+
+	fmt.Println("p - ", p)
+	fmt.Println("q - ", q)
+
+	//q = 11
+}
+
+// a CONSTANT is a simple unchanging value
